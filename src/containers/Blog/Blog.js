3d2a1b8d8094599548407bc152383075619208a8
Kenny Drobnack <kennydrobnack@gmail.com>
2018-04-11 06:40:24 -0400

    Dynamically load posts from server

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,21 +7,35 @@ import NewPost from '../../components/NewPost/NewPost'
 import './Blog.css'
 
 class Blog extends Component {
+    state = {
+        posts: []
+    }
+
     componentDidMount () {
         axios.get('https://jsonplaceholder.typicode.com/posts').then(
             response => {
-                console.log(response)
+                const posts = response.data.slice(0, 4)
+                const updatedPosts = posts.map( post => {
+                    return {
+                        ...post, 
+                        author: 'Max'
+                    }
+                })
+                this.setState({ posts: updatedPosts})
+                console.log(posts)
             }
         )
     }
 
     render () {
+        const posts = this.state.posts.map(post => {
+            return <Post title={post.title} key={post.id} author={post.author}/>
+        })
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
