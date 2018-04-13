c9e21a6367e3dcd0c3f176d713faa72ea17220a4
Kenny Drobnack <kennydrobnack@gmail.com>
2018-04-13 06:51:19 -0400

    Added axios request and response interceptors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+axios.interceptors.request.use(request => {
+    console.log("REQUEST from interceptor: ", request)
+    return request
+}, error => {
+    console.log("ERROR from interceptor: ", error)
+    return Promise.reject(error)
+})
+
+axios.interceptors.response.use(response => {
+    console.log("RESPONSE from interceptor: ", response)
+    return response
+}, error => {
+    console.log("ERROR from interceptor: ", error)
+    return Promise.reject(error)
+})
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
