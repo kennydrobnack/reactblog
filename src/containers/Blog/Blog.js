8d335ee2a290944c29b37992fe7ab2d2827904bf
Kenny Drobnack <kennydrobnack@gmail.com>
2018-05-20 22:12:33 -0400

    Using hoc component to only load component when needed

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
 import Posts from './Posts/Posts'
-import NewPost from './NewPost/NewPost'
+// import NewPost from './NewPost/NewPost'
+import asyncComponent from '../../hoc/aysncComponent'
 import './Blog.css'
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom'
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost')
+})
 
 class Blog extends Component {
     render() {
@@ -20,10 +25,10 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    <Route path="/new-post" component={NewPost}/>
+                    <Route path="/new-post" component={AsyncNewPost}/>
                     <Route path="/posts" component={Posts}/>
-                    <Route render={() => <h1>Not found</h1>}/>
-                    {/* <Redirect from="/" to="/posts" /> */}
+                    {/* <Route render={() => <h1>Not found</h1>}/> */}
+                    <Redirect from="/" to="/posts" />
                 </Switch>
             </div>
         )
